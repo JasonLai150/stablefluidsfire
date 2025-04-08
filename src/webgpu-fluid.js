83a05a85dbcb81d9079ca41369e06e46a693bb39
JasonLai150 <laijason150@gmail.com>
2025-04-08 01:38:12 -0400

    gpubufferusage const

--- src/webgpu-fluid.js
+++ src/webgpu-fluid.js
@@ -15,6 +15,19 @@ const GPUTextureUsage = window.GPUTextureUsage || {
     STORAGE_BINDING: 0x08,
     RENDER_ATTACHMENT: 0x10
 };
+
+const GPUBufferUsage = window.GPUBufferUsage || {
+  MAP_READ: 0x0001,
+  MAP_WRITE: 0x0002,
+  COPY_SRC: 0x0004,
+  COPY_DST: 0x0008,
+  INDEX: 0x0010,
+  VERTEX: 0x0020,
+  UNIFORM: 0x0040,
+  STORAGE: 0x0080,
+  INDIRECT: 0x0100,
+  QUERY_RESOLVE: 0x0200
+};
   
 const SIM_WIDTH = 128;
 const SIM_HEIGHT = 256;
